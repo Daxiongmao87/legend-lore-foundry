10a855eb2a48b2952d486d5965be0167b0f5d39b
Daxiongmao87 <Daxiongmao87@gmail.com>
2024-01-24 22:08:10 -0600

    Update utils.js

--- scripts/module/utils.js
+++ scripts/module/utils.js
@@ -6,23 +6,24 @@
  * @param {string[]} [options.type=['info']] - The type of log message (e.g., 'info', 'warn', 'error').
  * @param {Error} [options.error] - An optional error object to log.
  */
-export function log(options = {
+export function log(params = {
     message,
-    display: ["console"],
-    type: ["info"],
+    display,
+    type,
     error
     }) {
-    const console_message = `Legend Lore | ${message}`
-    const ui_message = `${message}`
-    if (options.type.contains("console")) {
-        if (options.type.contains("info")) {
+    const options = Object.assign({ display: ["console"], type: ["info"] }, params);
+    const console_message = `Legend Lore | ${options.message}`
+    const ui_message = `${options.message}`
+    if (options.display.includes("console")) {
+        if (options.type.includes("info")) {
             console.info(console_message);
         }
-        if (options.type.contains("warn")) {
+        if (options.type.includes("warn")) {
             console.warn(console_message);
         }
-        if (options.type.contains("error")) {
-            if(options.error){ 
+        if (options.type.includes("error")) {
+            if(options.error){
                 console.error(console_message, options.error);
             }
             else {
@@ -30,20 +31,20 @@ export function log(options = {
             }
         }
     }
-    if (options.type.contains("ui")) {
-        if (options.type.contains("info")) {
+    if (options.display.includes("ui")) {
+        if (options.type.includes("info")) {
             ui.notifications.info(ui_message);
         }
-        if (options.type.contains("warn")) {
+        if (options.type.includes("warn")) {
             ui.notifications.warn(ui_message);
         }
-        if (options.type.contains("error")) {
-            if(options.error){ 
+        if (options.type.includes("error")) {
+            if(options.error){
                 ui.notifications.error(ui_message);
             }
             else {
                 ui.notifications.error(ui_message + " [See web console for more information]");
             }
-        }            
+        }
     }
 }
